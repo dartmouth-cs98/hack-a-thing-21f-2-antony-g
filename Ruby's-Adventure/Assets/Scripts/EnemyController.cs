4335a98e8187e7eca2730729311144620391e081
Antonyg99 <Antony.L.Guzman.22@dartmouth.edu>
2021-09-29 12:56:33 -0400

    added particles and health bar

--- Ruby's-Adventure/Assets/Scripts/EnemyController.cs
+++ Ruby's-Adventure/Assets/Scripts/EnemyController.cs
@@ -8,18 +8,29 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2d;
     float timer;
     int direction = 1;
+    bool broken = true;
+
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+
+        if (!broken)
+        {
+            return;
+        }
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -31,14 +42,24 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        if (!broken)
+        {
+            return;
+        }
+
         Vector2 position = rigidbody2d.position;
 
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
         }
         else {
             position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("MoveX", direction);
+            animator.SetFloat("MoveY", 0);
         }
         rigidbody2d.MovePosition(position);
         
@@ -51,5 +72,12 @@ public class EnemyController : MonoBehaviour
         if (player != null) {
             player.ChangeHealth(-1);
         }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2d.simulated = false;
+        smokeEffect.Stop();
     }
 }
